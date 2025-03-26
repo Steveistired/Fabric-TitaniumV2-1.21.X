8d20607dacb4cbce6c960618d36953bf5e4d384e
Steve <legiaphuchcm@gmail.com>
2025-03-25 20:49:41 -0600

    E

---
+++ src/main/java/net/stevele/titaniumv2/TitaniumV2.java
@@ -0,0 +1,16 @@
+package net.stevele.titaniumv2;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TitaniumV2 implements ModInitializer {
+	public static final String MOD_ID = "titanium-v2";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/stevele/titaniumv2/TitaniumV2Client.java
@@ -0,0 +1,10 @@
+package net.stevele.titaniumv2;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TitaniumV2Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
