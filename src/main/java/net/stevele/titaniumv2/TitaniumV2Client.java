1d5e1a5a6845342c25b1b8aca47b67ba0d6ae935
Steveistired <legiaphuchcm@gmail.com>
2025-06-06 08:49:21 -0600

    Update TitaniumV2Client.java

--- src/main/java/net/stevele/titaniumv2/TitaniumV2Client.java
+++ src/main/java/net/stevele/titaniumv2/TitaniumV2Client.java
@@ -2,6 +2,8 @@ package net.stevele.titaniumv2;
 
 import net.fabricmc.api.ClientModInitializer;
 
+//Allow the client to launch with the mod
+
 public class TitaniumV2Client implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
