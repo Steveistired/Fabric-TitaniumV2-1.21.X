5eaa0260e234d2bab9ccc17abf032197596e6360
Steveistired <legiaphuchcm@gmail.com>
2025-06-06 08:49:52 -0600

    Update TitaniumV2.java

--- src/main/java/net/stevele/titaniumv2/TitaniumV2.java
+++ src/main/java/net/stevele/titaniumv2/TitaniumV2.java
@@ -15,6 +15,8 @@ import net.stevele.titaniumv2.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//Codes to register everything
+
 public class TitaniumV2 implements ModInitializer {
 	public static final String MOD_ID = "titanium-v2";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
